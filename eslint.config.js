70fe837cde97ff536f091532055a40f69d907e24
Kasih <zufarfauzan32@gmail.com>
2026-07-26 00:17:14 +0800

    ignore db

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,4 @@ export default [
             "semi": ["error", "always"]
         }
     }
-]
+];
--- src/app/web.js
+++ src/app/web.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { errMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../routes/public-routes.js";
-import { onlyLoginRouter } from "../routes/private-routes.js";
 
 const web = express();
 
@@ -9,7 +8,6 @@ const web = express();
 web.use(express.json());
 
 web.use(publicRouter);
-web.use(onlyLoginRouter);
 
 web.use(errMiddleware);
 
